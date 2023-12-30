5512ad7d7fae0af60bf17894239016cc8665c5a5
arnabg95 <arnab.gupta@weavers-web.com>
2023-12-30 23:44:07 +0530

    setup done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <center>
+        <h1>working</h1>
+      </center>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+import { nextui } from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  // ...
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const darkMode = "class";
+export const plugins = [nextui()];
